85d062d1e5b309af1adfabb7ef314fafe49afff9
zacaj <github@zacaj.com>
2019-06-24 18:51:34 -0400

    solenoid-16: rewrite modes, triggers work

--- solenoid-16.X/solenoid.c
+++ solenoid-16.X/solenoid.c
@@ -1,89 +1,148 @@
 #include "../test3.X/sat.c"
 void error() {
-    
+
 }
+
+typedef enum {
+    Disabled = 0,
+    Input = 1,
+    Momentary = 2,
+    OnOff = 3,
+    Triggered = 4,
+} Mode;
 
 #define ON 0
 #define OFF 1
 
 typedef struct {
     Pin pin;
-    uint32_t onTime, offTime;
-    uint32_t maxOnTime, minOnTime; //0=disabled
-    uint8_t triggers; // if !0, this is an input that triggers solenoid \triggers-1\
-    
-    uint32_t dontFireBefore;
-    uint32_t onSince;
-    uint8_t triggered;
+    Mode mode;
+
+    uint32_t cooldownTime; // if !0, how long it must be off before allowing another trigger (in OnOff only used after max is reached)
+
+    // Triggered:
+    uint8_t triggeredBy; // number of the solenoid that will trigger it
+    uint32_t minOnTime;
+    // maxOnTime
+
+    // momentary:
+    uint32_t onTime;
+
+    // on off
+    uint32_t maxOnTime; // if !0, max time it can stay on before cooldown activtates
+
+    uint32_t dontFireBefore; // if !0, will block activation before this time.  if -1 disabled while input is on
+    uint32_t onSince; // if !0, the solenoid is on, and has been since this time
 } Solenoid;
 
+#define SOL_DEFAULT(port, pin) { { port, pin }, Disabled, 0, -1, 0, 50, 0, 0, 0}
 //note v4 pins
 Solenoid solenoid[16] = {
-    { { IOPORT_B, p6 , 0}, 25, 250, 0, 0, 0, 0, 0 , 0},
-    { { IOPORT_B, p7 , 0}, 25, 250, 0, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p8 , 0}, 25, 250, 0, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p11 , 0}, 25, 250, 0, 0, 0,0, 0 , 0}, // low power + connector
-    { { IOPORT_B, p9 , 0}, 25, 250, 0, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p13 , 0}, 25, 250, 0, 0, 0,0, 0 , 0},
-    
-    { { IOPORT_B, p0 , 0}, 25, 250, 0, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p1 , 0}, 25, 250, 0, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p2 , 0}, 25, 250, 100, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p3 , 0}, 25, 250, 100, 0, 0,0, 0 , 0}, //low power
-    { { IOPORT_A, p3 , 0}, 25, 250, 100, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p4 , 0}, 25, 250, 100, 0, 0,0, 0 , 0},
-    
-    { { IOPORT_B, p12 , 0}, 25, 250, 100, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p10 , 0}, 25, 250, 100, 0, 0,0, 0 , 0}, // low power + connector
-    { { IOPORT_A, p4 , 0}, 25, 250, 100, 0, 0,0, 0 , 0},
-    { { IOPORT_B, p5 , 0},25, 250, 100, 0, 0,0, 0 , 0}, // low power
+    SOL_DEFAULT(IOPORT_B, p6),
+    SOL_DEFAULT(IOPORT_B, p7),
+    SOL_DEFAULT(IOPORT_B, p8),
+    SOL_DEFAULT(IOPORT_B, p11), // low power + connector
+    SOL_DEFAULT(IOPORT_B, p9),
+    SOL_DEFAULT(IOPORT_B, p13),
+
+    SOL_DEFAULT(IOPORT_B, p0),
+    SOL_DEFAULT(IOPORT_B, p1),
+    SOL_DEFAULT(IOPORT_B, p2),
+    SOL_DEFAULT(IOPORT_B, p3), //low power
+    SOL_DEFAULT(IOPORT_A, p3),
+    SOL_DEFAULT(IOPORT_B, p4),
+
+    SOL_DEFAULT(IOPORT_B, p12),
+    SOL_DEFAULT(IOPORT_B, p10), // low power + connector
+    SOL_DEFAULT(IOPORT_A, p4),
+    SOL_DEFAULT(IOPORT_B, p5), // low power
 };
 
 
-uint32_t turnOffSolenoid(Solenoid *s) {  
-    if(s->triggers) return 0;
-    if(s->minOnTime && s->onSince && msElapsed-s->onSince < s->minOnTime) {
-        return 0;
+uint32_t turnOffSolenoid(Solenoid *s) {
+    switch(s->mode) {
+        case Input:
+        case Disabled:
+            return 0;
+        case Momentary:
+            if (s->cooldownTime) {
+                s->dontFireBefore = msElapsed + s->cooldownTime;
+            }
+            break;
+        case Triggered:
+            if (s->cooldownTime && s->cooldownTime != -1) {
+                s->dontFireBefore = msElapsed + s->cooldownTime;
+            }
+            if(s->minOnTime && s->onSince && msElapsed-s->onSince < s->minOnTime) {
+                return 0;
+            }
+            break;
+        case OnOff:
+            break;
     }
-    
+
     setOut(s->pin, OFF);
-    s->triggered = 0;
     s->onSince = 0;
     return 0;
 }
 
-uint32_t fireSolenoid(Solenoid *s) {
-    if(s->triggers) return 0;
-    if(s->triggered) return 0;
+void turnOnSolenoid(Solenoid *s) {
+    switch(s->mode) {
+        case Input:
+        case Disabled:
+            break;
+        case Momentary:
+        case Triggered:
+        case OnOff:
+            if(msElapsed < s->dontFireBefore) return;
+            if(s->onSince) return;
+
+            setOut(s->pin, ON);
+            s->onSince = msElapsed;
+            break;
+    }
+}
+
+uint32_t fireSolenoid(Solenoid *s, uint32_t onTime) {
+    switch(s->mode) {
+        case Input:
+        case Disabled:
+        case Triggered:
+        case OnOff:
+            return 0;
+        case Momentary:
+            if (msElapsed < s->dontFireBefore) {
+                return 0;
+            }
+            if (s->onSince)
+                return 0;
+            break;
+    }
+    // only momentary
+    if (!onTime)
+        onTime = s->onTime;
     
-    if(msElapsed < s->dontFireBefore)
-        callIn(fireSolenoid, s, s->dontFireBefore-msElapsed);
-    else {
-        s->triggered = 0;
-        setOut(s->pin, ON);
-        s->dontFireBefore = msElapsed + s->onTime + s->offTime;
-        if(!callIn(turnOffSolenoid, s, s->onTime)) {
-            setOut(s->pin, OFF);
-        }
+    turnOnSolenoid(s);
+    
+    if(!callIn(turnOffSolenoid, s, onTime)) {
+        setOut(s->pin, OFF);
     }
     return 0;
 }
 
-void turnOnSolenoid(Solenoid *s) {
-    if(s->triggers) return;
-    if(msElapsed < s->dontFireBefore) return;
-    if(s->onSince) return;
-    
-    setOut(s->pin, ON);
-    s->onSince = msElapsed;
-}
-
 void initSolenoid(Solenoid *s) {
-    if(s->triggers) {
-        initIn(s->pin);
-    }
-    else {
-        initOut(s->pin, OFF);
+    switch(s->mode) {
+        case Input:
+            initIn(s->pin);
+            break;
+        case Disabled:
+            initIn(s->pin);
+            break;
+        case OnOff:
+        case Momentary:
+        case Triggered:
+            initOut(s->pin, OFF);
+            break;
     }
 }
 
@@ -111,26 +170,23 @@ uint8_t commandReceived(uint8_t* cmd, uint8_t len) {
             switch(cmd[0]>>4) {
                 case Ob(0000): {//fire solenoid
                     uint8_t n = cmd[0]&0xF;
-                    fireSolenoid(&solenoid[n]);
+                    fireSolenoid(&solenoid[n], 0);
                     break;
                 }
                 case Ob(0001): { //fire custom
                     LEN(4);
                     uint8_t n = cmd[0]&0xF;
                     uint32_t on = cmd[1];
-                    uint32_t off = (cmd[2]<<8)|cmd[3];
                     Solenoid *s = &solenoid[n];
-                    if(s->triggers) break;
-                    setOut(s->pin, ON);
-                    s->dontFireBefore = msElapsed + on + off;
-                    callIn(turnOffSolenoid, s, on);
+                    fireSolenoid(s, on);
                     break;
                 }
                 case Ob(0010): { //fire in
                     LEN(3);
                     uint8_t n = cmd[0]&0xF;
                     uint32_t in = (cmd[1]<<8)|cmd[2];
-                    callIn(fireSolenoid, (n), in);
+                    Solenoid *s = &solenoid[n];
+                    callIn(fireSolenoid, s, in);
                     break;
                 }
                 case Ob(0011): { //turn on solenoid
@@ -146,14 +202,30 @@ uint8_t commandReceived(uint8_t* cmd, uint8_t len) {
                     break;
                 }
                 case Ob(0110): { //conf
-                    LEN(8);
-                    uint8_t n = cmd[0]&Ob(111);
+                    uint8_t n = cmd[0]&0xF;
                     Solenoid *s = &solenoid[n];
-                    s->onTime = cmd[1];
-                    s->offTime = (cmd[2]<<8)|cmd[3];
-                    s->maxOnTime = (cmd[4]<<8)|cmd[5];
-                    s->minOnTime = cmd[6];
-                    s->triggers = cmd[7];
+                    Mode mode = (cmd[0]&0xF0)>>4;
+                    switch (mode) {
+                        case Disabled:
+                        case Input:
+                            break;
+                        case Triggered:
+                            LEN(14);
+                            s->triggeredBy = cmd[5];
+                            s->minOnTime = read32(7);
+                            s->maxOnTime = read32(8);
+                            break;
+                        case Momentary:
+                            LEN(9);
+                            s->onTime = read32(5);
+                            break;
+                        case OnOff:
+                            LEN(9);
+                            s->maxOnTime = read32(5);
+                            break;
+                    }
+                    s->mode = mode;
+                    s->cooldownTime = read32(1);
                     initSolenoid(s);
                     break;
                 }
@@ -169,7 +241,7 @@ void loop() {
     uint32_t ms = msElapsed+1;
     if(msElapsed-last>5000) {
         last = msElapsed;
-        
+
         /*if(state) {
            // uint8_t cmd = Ob(01010001);
             uint8_t cmd = Ob(01011001);
@@ -184,82 +256,69 @@ void loop() {
             commandReceived(&cmd, 1);
         }*/
         //fireSolenoid(&solenoid[0]);
-        
+
         state=!state;
     }
     
-    for(int i=0; i<16; i++) {
-        Solenoid *s = &solenoid[i];
-        if(s->triggers) {
-            if(getIn(s->pin)) {
-                turnOnSolenoid(&solenoid[s->triggers-1]);
-                solenoid[s->triggers-1].triggered = 1;
-            }
-            else if(solenoid[s->triggers-1].triggered) {
-                turnOffSolenoid(&solenoid[s->triggers-1]);
-            }
-        }
-        else {
-            if(s->onSince && s->maxOnTime) {
-                if(ms - s->onSince>s->maxOnTime) {
-                    turnOffSolenoid(s);
-                    s->dontFireBefore = msElapsed + 10000;
+    for (int i=0; i<16; i++) {
+        Solenoid* s = &solenoid[i];
+        switch(s->mode) {
+            case Disabled:
+            case Input:
+            case Momentary:
+                break;
+            case OnOff:
+                if (s->onSince) {
+                    if (s->maxOnTime && msElapsed-s->onSince > s->maxOnTime) {
+                        turnOffSolenoid(s);
+                    }
                 }
+                break;
+            case Triggered: {
+                if (s->triggeredBy >= 16)
+                    break;
+                Solenoid* trigger = &solenoid[s->triggeredBy];
+                uint8_t input = getIn(trigger->pin);
+                if (!input) {
+                    if (s->cooldownTime == -1)
+                        s->cooldownTime = 0;
+                    turnOffSolenoid(s);
+                }
+                else {
+                    if (s->onSince) { // already on
+                        if (s->maxOnTime && msElapsed - s->onSince > s->maxOnTime) {
+                            turnOffSolenoid(s);
+                            if (!s->cooldownTime) {
+                                s->cooldownTime = -1;
+                            }
+                        }
+                    }
+                    else if (s->cooldownTime != -1) {
+                        turnOnSolenoid(s);
+                    }
+                }
+                break;
             }
+                
         }
     }
-    
-    /*
-    for(int i=0; i<4; i++) {
-        if(sSolenoid[i].turnedOnAt) {
-            if(ms - sSolenoid[i].turnedOnAt>sSolenoid[i].maxOnTime) {
-                initOut(sSolenoid[i].pin, 0);
-                setOut(specialDisable[i>=4? 1:0], 1);
-            }
-            /*else if(msElapsed - sSolenoid[i].turnedOnAt>sSolenoid[i].minOnTime) {
-                initIn(sSolenoid[i].pin);
-            }*
-        }
-        if(!sSolenoid[i].turnedOnAt && getIn(sSolenoid[i].pin)) {
-            sSolenoid[i].turnedOnAt = ms;
-        }
-        if(sSolenoid[i].turnedOnAt && !getIn(sSolenoid[i].pin) && ms - sSolenoid[i].turnedOnAt>5) {
-            if(ms - sSolenoid[i].turnedOnAt > sSolenoid[i].maxOnTime) {
-                //if(msElapsed - sSolenoid[i].turnedOnAt > sSolenoid[i].maxOnTime + sSolenoid[i].offTime) {
-                    initIn(sSolenoid[i].pin);
-                    if(!specialDisabled[i>=4?1:0])
-                        setOut(specialDisable[i>=4? 1:0], 0);
-                    sSolenoid[i].turnedOnAt = 0;
-               // }
-            }
-            else {
-                /*if(sSolenoid[i].minOnTime && 
-                    msElapsed - sSolenoid[i].turnedOnAt < sSolenoid[i].minOnTime) {
-                    initOut(sSolenoid[i].pin, 1);
-                }
-                else *
-                    sSolenoid[i].turnedOnAt = 0;
-            }
-        }
-    }*/
+
 }
 
 void init() {
     CNPDB=0;
-    for(int i=0; i<16; i++) {
-        initOut(solenoid[i].pin, 0);
-        solenoid[i].onTime = 25;
-        solenoid[i].offTime = 100;
-        solenoid[i].maxOnTime = 100;
-        solenoid[i].minOnTime = 100;
-        solenoid[i].onSince = 0;
-        solenoid[i].dontFireBefore = 0;
-    }
-    solenoid[15].triggers = 1;
+    
+    solenoid[0].mode = Triggered;
+    solenoid[0].minOnTime = 100;
+    solenoid[0].maxOnTime = 3000;
+    solenoid[0].triggeredBy = 15;
+
+    solenoid[15].mode = Input;
+
     for(int i=0; i<16; i++) {
         initSolenoid(&solenoid[i]);
     }
-    
+
 }
 
 void crashed() {
